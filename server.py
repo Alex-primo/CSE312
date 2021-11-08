701a4ac37afcefedfeebb28e8f1c7cb910884086
rwwilkow <rwwilkow@buffalo.edu>
2021-11-08 13:56:23 -0500

    Starting Server

--- server.py
+++ server.py
@@ -1,1 +1,26 @@
+import socketserver
+import secrets
+import hashlib
+import base64
+import pymongo
+import json
+
+
+
+
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        received_data = self.request.recv(2048).strip()
+        print(self.client_address[0] + " is sending data:")
+        print(received_data)
+        
+
+
+if __name__ == "__main__":
+    HOST, PORT = "0.0.0.0", 8000
+    
+    server = socketserver.ThreadingTCPServer((HOST,PORT), MyTCPHandler)
+    server.serve_forever()
 
