53836154ca4d92bc8a13627c581fbe6469228404
rwwilkow <rwwilkow@buffalo.edu>
2021-11-08 14:04:34 -0500

    Basic login page

--- server.py
+++ server.py
@@ -15,6 +15,19 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         received_data = self.request.recv(2048).strip()
         print(self.client_address[0] + " is sending data:")
         print(received_data)
+
+        with open("login.html", "rb") as file:
+            b = file.read()
+            print("HIIIIIIIIIIIIIIIII")
+            print(b)
+                
+            respond = "HTTP/1.1 200 OK\r\n"
+            respond += "Content-Type: text/html; charset=utf-8\r\n"
+            respond += "X-Content-Type-Options: nosniff\r\n"
+            respond += "ContentLength: " + str(len(b)) + "\r\n"
+            respond += "\r\n"
+            print("I'm Sending Unwanted data", respond)
+            self.request.sendall(respond.encode() + b)
         
 
 
